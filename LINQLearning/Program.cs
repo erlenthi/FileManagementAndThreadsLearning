d8d30ab9a1cd254c19cbe3de65efb4c54582affa
erlenthi <51139837+erlenthi@users.noreply.github.com>
2026-01-15 15:49:45 +0100

    Add generated NuGet files for LINQLearning project
    - Created LINQLearning.csproj.nuget.g.props to define NuGet properties.
    - Added LINQLearning.csproj.nuget.g.targets for build targets.
    - Generated project.assets.json to manage project dependencies and assets.
    - Included project.nuget.cache for caching NuGet package information.

--- LINQLearning/Program.cs
+++ LINQLearning/Program.cs
@@ -12,6 +12,8 @@ var numbers = new List<List<int>>
 
 var allNumbers = new List<int>();
 
+allNumbers = numbers.SelectMany(number => number).ToList();
+
 foreach (var list in numbers)
 {
     foreach (var n in list)
@@ -20,54 +22,54 @@ foreach (var list in numbers)
     }
 }
 
-/// 
-/// 
-/// Exercise 2 — Files inside directories
-/// 
-var allFiles = new List<string>();
+// /// 
+// /// 
+// /// Exercise 2 — Files inside directories
+// /// 
+// var allFiles = new List<string>();
 
-foreach (var dir in Directory.GetDirectories("stores"))
-{
-    foreach (var file in Directory.GetFiles(dir, "*.json"))
-    {
-        allFiles.Add(file);
-    }
-}
-/////////////////Exercise 3 — Filtering while flattening
+// foreach (var dir in Directory.GetDirectories("stores"))
+// {
+//     foreach (var file in Directory.GetFiles(dir, "*.json"))
+//     {
+//         allFiles.Add(file);
+//     }
+// }
+// /////////////////Exercise 3 — Filtering while flattening
 
-var expensiveSales = new List<Sale>();
+// var expensiveSales = new List<Sale>();
 
-foreach (var store in stores)
-{
-    foreach (var sale in store.Sales)
-    {
-        if (sale.Amount > 1000)
-        {
-            expensiveSales.Add(sale);
-        }
-    }
-}
+// foreach (var store in stores)
+// {
+//     foreach (var sale in store.Sales)
+//     {
+//         if (sale.Amount > 1000)
+//         {
+//             expensiveSales.Add(sale);
+//         }
+//     }
+// }
 
-/////xercise 4 — Projection using outer + inner values
-var results = new List<string>();
+// /////xercise 4 — Projection using outer + inner values
+// var results = new List<string>();
 
-foreach (var store in stores)
-{
-    foreach (var employee in store.Employees)
-    {
-        results.Add($"{store.Name} - {employee.Name}");
-    }
-}
+// foreach (var store in stores)
+// {
+//     foreach (var employee in store.Employees)
+//     {
+//         results.Add($"{store.Name} - {employee.Name}");
+//     }
+// }
 
 
-////Exercise 5 — Many-to-many expansion
+// ////Exercise 5 — Many-to-many expansion
 
-var combinations = new List<(string store, string category)>();
+// var combinations = new List<(string store, string category)>();
 
-foreach (var store in stores)
-{
-    foreach (var category in categories)
-    {
-        combinations.Add((store.Name, category));
-    }
-}
+// foreach (var store in stores)
+// {
+//     foreach (var category in categories)
+//     {
+//         combinations.Add((store.Name, category));
+//     }
+// }
